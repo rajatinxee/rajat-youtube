aaaca8c3a3166d0f919ca390afe588a34db7b86b
RajatPanwal <rajatpanwal@gmail.com>
2025-12-28 21:21:43 +0530

    send live message

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -5,7 +5,7 @@ const ChatMessage = ({ name, message }) => {
     <div className="flex items-center">
       <img src={USER_ICON} alt="USER_ICON" className="h-8 rounded-full" />
       <span className="font-bold mx-3 text-gray-700">{name}</span>
-      <span className="text-gray-500">"{message}"</span>
+      <span className="text-gray-500">{message}</span>
     </div>
   );
 };
--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/Redux/Slices/chatSlice";
@@ -6,6 +6,8 @@ import generateRandomNames, { generateRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
   const dispatch = useDispatch();
+
+  const [liveMessage, setLiveMessage] = useState("");
 
   const ChatMessages = useSelector((store) => store.chat.messages);
 
@@ -18,17 +20,54 @@ const LiveChat = () => {
           message: generateRandomMessage(20) + " 🤪",
         })
       );
-    }, 1000);
+    }, 2000);
 
     return () => clearInterval(timer);
   }, []);
 
+  const sendLiveMessage = () => {
+    if (liveMessage === "") return;
+
+    dispatch(
+      addMessage({
+        name: "Rajat",
+        message: liveMessage,
+      })
+    );
+    setLiveMessage("");
+  };
+
   return (
-    <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse  ">
-      <span className="text-red-500 font-bold">Live Chat</span>
-      {ChatMessages.map((c, i) => (
-        <ChatMessage key={i} name={c.name} message={c.message} />
-      ))}
+    <div>
+      <div className="w-full h-[575px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse  ">
+        <span className="text-red-500 font-bold">Live Chat</span>
+        {ChatMessages.map((c, i) => (
+          <ChatMessage key={i} name={c.name} message={c.message} />
+        ))}
+      </div>
+      <div className="w-full p-2 ml-2 border border-black rounded-md">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            sendLiveMessage();
+          }}
+        >
+          <input
+            type="text"
+            className="w-72 border border-gray-500 rounded-md p-2"
+            value={liveMessage}
+            onChange={(e) => setLiveMessage(e.target.value)}
+            placeholder="Send a live chat..."
+          />
+
+          <button
+            type="submit"
+            className="bg-green-700 font-bold text-white m-2 p-2 rounded-lg hover:bg-gray-600 transition-all duration-200"
+          >
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
